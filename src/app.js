1378782d1eebe60c374cdd45c0ea11a840ae5d27
tushargithub52 <tusharraiku6020@gmail.com>
2024-10-12 01:11:00 +0530

    user routes and controllers

--- src/app.js
+++ src/app.js
@@ -19,4 +19,14 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)   
+//https://localhost:8000/users/register
+
+
 export { app }
