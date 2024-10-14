e6c98e138295d5e82a2df56c7b7bbc3bb872c8a6
tushargithub52 <tusharraiku6020@gmail.com>
2024-10-14 23:18:34 +0530

    added subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -181,7 +181,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 export const refreshAccessToken = asyncHandler( async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "Unauthorized Request")
     }
 
